da9463e14f38bd7241ce9a9bdb52333a063c88a1
Kishore-s-19 <kdkishore315@gmail.com>
2026-01-09 02:00:24 +0530

    JWT Added and APP running

--- src/main/java/com/groomup/backend/repository/UserRepository.java
+++ src/main/java/com/groomup/backend/repository/UserRepository.java
@@ -1,6 +1,17 @@
-
 package com.groomup.backend.repository;
 
-public class UserRepository {
+import com.groomup.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByPhone(String phone);
 }
